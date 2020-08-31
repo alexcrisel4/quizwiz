df51f4c81dcdded32a4363f1bcc132aaf5b4c2be
Alexander <alexcrisel4@gmail.com>
2020-08-31 16:14:15 -0700

    completes sessions and application controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController 
+  def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if user.nil? 
+      render json: "Incorrect email or password"
+    else 
+      login!(user)
+      redirect_to user_url
+    end
+  end
+
+  def destroy 
+    logout!
+    redirect_to new_session_url 
+  end 
+
+
+end
