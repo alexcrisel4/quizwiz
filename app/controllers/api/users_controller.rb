e52ba1416370cb7ca877cff6b2d8dc7c0b321732
Alexander <alexcrisel4@gmail.com>
2020-08-31 16:40:26 -0700

    completes backend routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController 
+  before_action :require_current_user!, except: [:create, :new]
+
+  def create 
+    @user = User.new(params[:user])
+    if @user.save
+      login!(@user)
+      redirect_to user_url
+    else 
+      render json: @user.errors.full_messages
+    end
+  end
   
+  # def show
+  #   @user = User.find_by(params[:user][:id])
+  # end
+  
+  
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :show, :destroy, :update]
+  resource :session, only: [:create, :destroy]
+  resources :quizzes do 
+    resources :questions do 
+      resources :answers 
+      end 
+    end 
+  
 end
