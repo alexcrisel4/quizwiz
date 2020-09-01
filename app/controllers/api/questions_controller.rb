2fb2d50290700fd5488f38ab5ccd0ff78f059b85
Alexander <alexcrisel4@gmail.com>
2020-09-01 15:16:00 -0700

    Finish questions controller

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,16 +1,35 @@
 class Api::QuestionsController < ApplicationController 
   def create 
+    @question = Question.new(question_params)
+    if(@question.save)
+      render :show
+    else 
+      render json: @question.errors.full_messages
+    end
   end 
 
   def show 
+    @question = Question.find(params[:id])
   end 
 
   def update 
+    @question = Question.find(params[:id])
+    if(@question.update(question_params))
+      render :show 
+    else  
+      render json: @question.errors.full_messages 
+    end
   end 
 
-  def index 
-  end 
+  
 
   def destroy 
+    @question = Question.find(params[:id])
+    @question.destroy
+   
+  end 
+
+  def question_params
+    params.require(:question).permit(:id, :body)
   end
 end
