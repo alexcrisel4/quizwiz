c0333bfdc5956242216c3cb19e1c68f5e22ed4e6
Alexander <alexcrisel4@gmail.com>
2020-09-01 15:20:50 -0700

    Complete answers controller

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,16 +1,34 @@
 class Api::AnswersController < ApplicationController 
   def create 
+    @answer = Answer.new(answer_params)
+    if(@answer.save)
+      render :show
+    else 
+      render json: @answer.errors.full_messages
+    end
   end 
 
   def show 
+    @answer = Answer.find(params[:id])
   end 
 
   def update 
+    @answer = Answer.find(params[:id])
+    if(@answer.update(answer_params))
+      render :show 
+    else  
+      render json: @answer.errors.full_messages 
+    end
   end 
 
-  def index 
-  end 
 
   def destroy 
-  end
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+  end 
+
+  def answer_params
+    params.require(:answer).permit(:id, :body, :correct)
+  end 
+
 end
