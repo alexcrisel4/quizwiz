731e93ccd3adae0322bd1000851305c0bb3a4e0c
Alexander <alexcrisel4@gmail.com>
2020-09-24 15:42:27 -0700

    correct root page to routes.rb

--- app/frontend/quizwiz.jsx
+++ app/frontend/quizwiz.jsx
@@ -3,25 +3,29 @@ import ReactDOM from "react-dom";
 import Root from './components/root';
 import configureStore from "./store/store";
 import {signup} from './util/session_util';
-window.signup = signup;
+// window.signup = signup;
 
 document.addEventListener("DOMContentLoaded", () => {
 
 
   
-  let store;
-  if (window.currentUser) {
-    const preloadedState = {
-      entities: {
-        users: { [window.currentUser.id]: window.currentUser }
-      },
-      session: { id: window.currentUser.id }
-    };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
+  // let store;
+  const store = configureStore();
+  // if (window.currentUser) {
+  //   const preloadedState = {
+  //     entities: {
+  //       users: { [window.currentUser.id]: window.currentUser }
+  //     },
+  //     session: { id: window.currentUser.id }
+  //   };
+  //   store = configureStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+  //   store = configureStore();
+  // }
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store}/>, root);
