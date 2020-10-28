dc3d614db0ff894e13607d1f826da3e908f92d92
Alexander <alexcrisel4@gmail.com>
2020-10-28 12:58:24 -0700

    create session form

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+// import {AuthRoute, ProtectedRoute} from "../util/route_util.js";
+import SessionForm from "../components/session_form";
 
 const App = () => (
   <div>
     <h1>QUIZ WIZ</h1>
+    <SessionForm/>
   </div>
 );
 
---
+++ app/frontend/components/session_form.jsx
@@ -0,0 +1,49 @@
+
+import React, {useState} from "react";
+import {useDispatch} from "react-redux";
+import {signup, login} from "../actions/session_actions";
+import {useLocation} from "react-router-dom";
+
+export default () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const location = useLocation();
+  const formType = location.pathname.split("/")[1];
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let user
+    if(formType === "signup") {
+      user = { email, name: firstName + " " + lastName, password }
+      dispatch(signup(user))
+    } else {
+      user = { email, password}
+      dispatch(login(user))
+    }  
+  }
+
+
+  return(
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <label htmlFor="email">Email
+          <input placeholder="lizzobeemailing@gmail.com" type="text" value={email} onChange={e=>setEmail(e.currentTarget.value)}/>
+        </label>
+        {formType === "signup" ? <label htmlFor="first-name">Name
+          <input placeholder="Lizzo" type="text" value={firstName} onChange={e=>setFirstName(e.currentTarget.value)}/>
+        </label> : ""}
+        {formType === "signup" ? 
+        <label htmlFor="last-name">
+          <input type="text" value={lastName} onChange={e=>setLastName(e.currentTarget.value)}/>
+        </label> : "" }
+        <label htmlFor="password">Password
+          <input type="password" name="" id="" value={password} onChange={e=>setPassword(e.currentTarget.value)}/>
+        </label>
+       <input type="submit" value={formType === "signup" ? "Sign Up" : "Login"}/>
+      </form>
+    </div>
+  )
+}
