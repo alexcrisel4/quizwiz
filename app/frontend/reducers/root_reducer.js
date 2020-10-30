86b327d867e2dda3e506fe9acbfb6c61b2a0ccec
Alexander <alexcrisel4@gmail.com>
2020-10-30 11:17:47 -0700

    add quiz reducer to root

--- app/frontend/reducers/root_reducer.js
+++ app/frontend/reducers/root_reducer.js
@@ -2,8 +2,10 @@
 
 import { combineReducers } from "redux";
 import sessionReducer from "./session_reducer";
+import quizReducer from "./quiz_reducer"
 
 export default combineReducers({
-  session: sessionReducer
+  session: sessionReducer, 
+  quizzes: quizReducer
 })
 
