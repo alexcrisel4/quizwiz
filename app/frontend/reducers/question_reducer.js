51763cf7382b80234fa28622d2b21383a1910524
Alexander <alexcrisel4@gmail.com>
2020-11-04 09:39:01 -0800

    Questions reducer

---
+++ app/frontend/reducers/question_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_QUESTIONS, RECEIVE_QUESTION, REMOVE_QUESTION } from "../actions/question_actions";
+
+const questionsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return Object.assign({}, state, action.questions)
+    case RECEIVE_QUESTION:
+      return Object.assign({}, state, { [action.question.id]: action.question })
+    case REMOVE_QUESTION:
+      let newState = Object.assign({}, state);
+      delete newState[action.question.id]
+      return newState
+    default:
+      return state;
+  }
+}
+
+export default questionsReducer;
