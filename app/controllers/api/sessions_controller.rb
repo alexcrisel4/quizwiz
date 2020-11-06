936ccd56e1eb274213159b2b411708e838959d31
Raymond Leung <52337629+ray-leun1@users.noreply.github.com>
2020-11-06 03:34:26 -0800

    Adjust and format files for minor fixes and inclusion of logout button

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,19 +1,17 @@
-class Api::SessionsController < ApplicationController 
+class Api::SessionsController < ApplicationController
   protect_from_forgery with: :null_session
+
   def create
     user = User.find_by_credentials(params[:user][:email], params[:user][:password])
-    if user.nil? 
+    if user.nil?
       render json: "Incorrect email or password"
-    else 
+    else
       login!(user)
       render json: current_user
     end
   end
 
-  def destroy 
+  def destroy
     logout!
-    # redirect_to new_session_url 
-  end 
-
-
+  end
 end
